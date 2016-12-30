20220954d077f863d6820e33526a01291ad6b2ea
Skylar Moore <nickgray0@gmail.com>
2016-12-30 10:18:59 -0800

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,5 +1,6 @@
 #include "VapourSynth.h"
 #include "VSHelper.h"
+#include <cmath>
 #include <algorithm>
 #include <malloc.h>
 
@@ -8,6 +9,8 @@ struct FixFadesData final {
 	VSNodeRef *node = nullptr;
 	const VSVideoInfo *vi = nullptr;
 	int64_t mode = 0;
+	double threshold = 0.;
+	double color[3] = { 0., 0., 0. };
 	FixFadesData(const VSMap *in, const VSAPI *api) {
 		vsapi = api;
 		node = vsapi->propGetNode(in, "clip", 0, nullptr);
@@ -42,7 +45,7 @@ auto VS_CC fixfadesGetFrame(int n, int activationReason, void **instanceData, vo
 			auto width = vsapi->getFrameWidth(src, plane);
 			auto srcp = reinterpret_cast<const float **>(alloca(height * sizeof(void *)));
 			auto dstp = reinterpret_cast<float **>(alloca(height * sizeof(void *)));
-			auto TopFieldSum = 0., BottomFieldSum = 0.;
+			auto TopFieldSum = 0., BottomFieldSum = 0., CurrentBaseColor = d->color[plane];
 			auto Initialize = [=]() {
 				auto src_stride = vsapi->getStride(src, plane) / sizeof(float);
 				auto dst_stride = vsapi->getStride(dst, plane) / sizeof(float);
@@ -55,33 +58,33 @@ auto VS_CC fixfadesGetFrame(int n, int activationReason, void **instanceData, vo
 				for (auto y = 0; y < height; ++y)
 					if (y % 2 == false)
 						for (auto x = 0; x < width; ++x)
-							TopFieldSum += srcp[y][x];
+							TopFieldSum += srcp[y][x] - CurrentBaseColor;
 					else
 						for (auto x = 0; x < width; ++x)
-							BottomFieldSum += srcp[y][x];
+							BottomFieldSum += srcp[y][x] - CurrentBaseColor;
 			};
 			auto FixFadesMode0 = [&]() {
 				auto MeanSum = (TopFieldSum + BottomFieldSum) / 2.;
 				for (auto y = 0; y < height; ++y)
 					if (y % 2 == false)
 						for (auto x = 0; x < width; ++x)
-							dstp[y][x] = static_cast<float>(srcp[y][x] * MeanSum / TopFieldSum);
+							dstp[y][x] = static_cast<float>((srcp[y][x] - CurrentBaseColor) * MeanSum / TopFieldSum + CurrentBaseColor);
 					else
 						for (auto x = 0; x < width; ++x)
-							dstp[y][x] = static_cast<float>(srcp[y][x] * MeanSum / BottomFieldSum);
+							dstp[y][x] = static_cast<float>((srcp[y][x] - CurrentBaseColor) * MeanSum / BottomFieldSum + CurrentBaseColor);
 			};
 			auto FixFadesMode1 = [&]() {
 				auto MinSum = std::min(TopFieldSum, BottomFieldSum);
 				if (MinSum == TopFieldSum)
 					for (auto y = 1; y < height; y += 2)
 						for (auto x = 0; x < width; ++x) {
-							dstp[y][x] = static_cast<float>(srcp[y][x] * MinSum / BottomFieldSum);
+							dstp[y][x] = static_cast<float>((srcp[y][x] - CurrentBaseColor) * MinSum / BottomFieldSum + CurrentBaseColor);
 							dstp[y - 1][x] = srcp[y - 1][x];
 						}
 				else
 					for (auto y = 0; y < height; y += 2)
 						for (auto x = 0; x < width; ++x) {
-							dstp[y][x] = static_cast<float>(srcp[y][x] * MinSum / TopFieldSum);
+							dstp[y][x] = static_cast<float>((srcp[y][x] - CurrentBaseColor) * MinSum / TopFieldSum + CurrentBaseColor);
 							dstp[y + 1][x] = srcp[y + 1][x];
 						}
 			};
@@ -90,31 +93,41 @@ auto VS_CC fixfadesGetFrame(int n, int activationReason, void **instanceData, vo
 				if (MaxSum == TopFieldSum)
 					for (auto y = 1; y < height; y += 2)
 						for (auto x = 0; x < width; ++x) {
-							dstp[y][x] = static_cast<float>(srcp[y][x] * MaxSum / BottomFieldSum);
+							dstp[y][x] = static_cast<float>((srcp[y][x] - CurrentBaseColor) * MaxSum / BottomFieldSum + CurrentBaseColor);
 							dstp[y - 1][x] = srcp[y - 1][x];
 						}
 				else
 					for (auto y = 0; y < height; y += 2)
 						for (auto x = 0; x < width; ++x) {
-							dstp[y][x] = static_cast<float>(srcp[y][x] * MaxSum / TopFieldSum);
+							dstp[y][x] = static_cast<float>((srcp[y][x] - CurrentBaseColor) * MaxSum / TopFieldSum + CurrentBaseColor);
 							dstp[y + 1][x] = srcp[y + 1][x];
 						}
 			};
+			auto GetNormalizedDifference = [&]() {
+				auto FieldPixelCount = static_cast<int64_t>(width) * height / 2;
+				return std::abs(TopFieldSum - BottomFieldSum) / FieldPixelCount;
+			};
+			auto CopyToDestinationFrame = [=]() {
+				std::memcpy(dstp[0], srcp[0], width * height * sizeof(float));
+			};
 			Initialize();
 			FixFadesPrepare();
-			switch (d->mode) {
-			case 0:
-				FixFadesMode0();
-				break;
-			case 1:
-				FixFadesMode1();
-				break;
-			case 2:
-				FixFadesMode2();
-				break;
-			default:
-				break;
-			}
+			if (GetNormalizedDifference() < d->threshold)
+				CopyToDestinationFrame();
+			else
+				switch (d->mode) {
+				case 0:
+					FixFadesMode0();
+					break;
+				case 1:
+					FixFadesMode1();
+					break;
+				case 2:
+					FixFadesMode2();
+					break;
+				default:
+					break;
+				}
 		}
 		vsapi->freeFrame(src);
 		return dst;
@@ -143,10 +156,28 @@ auto VS_CC fixfadesCreate(const VSMap *in, VSMap *out, void *userData, VSCore *c
 		delete d;
 		return;
 	}
+	d->threshold = vsapi->propGetFloat(in, "threshold", 0, &err);
+	if (err)
+		d->threshold = 0.002;
+	if (d->threshold < 0.) {
+		vsapi->setError(out, "FixFades: threshold must not be negative!");
+		delete d;
+		return;
+	}
+	auto InputColorChannelCount = vsapi->propNumElements(in, "color");
+	if (InputColorChannelCount != -1) {
+		if (d->vi->format->numPlanes != InputColorChannelCount) {
+			vsapi->setError(out, "FixFades: Invalid color value for the input colorspace!");
+			delete d;
+			return;
+		}
+		for (auto i = 0; i < d->vi->format->numPlanes; ++i)
+			d->color[i] = vsapi->propGetFloat(in, "color", i, nullptr);
+	}
 	vsapi->createFilter(in, out, "FixFades", fixfadesInit, fixfadesGetFrame, fixfadesFree, fmParallel, 0, d, core);
 }
 
 VS_EXTERNAL_API(auto) VapourSynthPluginInit(VSConfigPlugin configFunc, VSRegisterFunction registerFunc, VSPlugin *plugin) {
 	configFunc("com.deinterlace.ftf", "ftf", "Fix Telecined Fades", VAPOURSYNTH_API_VERSION, 1, plugin);
-	registerFunc("FixFades", "clip:clip;mode:int:opt;", fixfadesCreate, 0, plugin);
+	registerFunc("FixFades", "clip:clip;mode:int:opt;threshold:float:opt;color:float[]:opt;", fixfadesCreate, nullptr, plugin);
 }

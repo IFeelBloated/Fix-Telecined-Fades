47357b348479c141a27a2b9a807dcb9981a66818
Skylar Moore <nickgray0@gmail.com>
2017-01-29 07:27:48 -0800

    Create cpu.asm

---
+++ cpu.asm
@@ -0,0 +1,35 @@
+; Copyright (C) 2005-2010 x264 project
+;
+; Authors: Loren Merritt <lorenm@u.washington.edu>
+;          Jason Garrett-Glaser <darkshikari@gmail.com>
+;
+; This file is part of VapourSynth.
+;
+; VapourSynth is free software; you can redistribute it and/or
+; modify it under the terms of the GNU Lesser General Public
+; License as published by the Free Software Foundation; either
+; version 2.1 of the License, or (at your option) any later version.
+;
+; VapourSynth is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+; Lesser General Public License for more details.
+;
+; You should have received a copy of the GNU Lesser General Public
+; License along with VapourSynth; if not, write to the Free Software
+; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+%include "x86inc.asm"
+
+SECTION .text
+
+cglobal _xgetbv, 3,7
+    push  r2
+    push  r1
+    mov  ecx, r0d
+    xgetbv
+    pop   r4
+    mov [r4], eax
+    pop   r4
+    mov [r4], edx
+    ret

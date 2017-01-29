2e0cdc2da1a247a64743227634d7dfe50584c143
Skylar Moore <nickgray0@gmail.com>
2017-01-28 23:04:34 -0800

    Update cpufeatures_gnu.hpp

--- cpufeatures_gnu.hpp
+++ cpufeatures_gnu.hpp
@@ -1,9 +1,9 @@
-#include <cpuid.h>
+#include <intrin.h>
 #include <immintrin.h>
 #include <cstdint>
 
-constexpr auto operator""_u32(unsigned long long val) {
-	return static_cast<uint32_t>(val);
+constexpr auto operator""_i32(unsigned long long val) {
+	return static_cast<int32_t>(val);
 }
 
 struct CPUFeatures final {
@@ -20,8 +20,12 @@ struct CPUFeatures final {
 	bool movbe = false;
 	bool popcnt = false;
 	CPUFeatures() {
-		auto eax = 0_u32, ebx = 0_u32, ecx = 0_u32, edx = 0_u32;
-		__get_cpuid(1, &eax, &ebx, &ecx, &edx);
+		decltype(0_i32) Registers[] = { 0_i32, 0_i32, 0_i32, 0_i32 };
+		auto &eax = Registers[0];
+		auto &ebx = Registers[1];
+		auto &ecx = Registers[2];
+		auto &edx = Registers[3];
+		__cpuid(Registers, 1);
 		can_run_vs = !!(edx & (1 << 26));
 		sse3 = !!(ecx & 1);
 		ssse3 = !!(ecx & (1 << 9));
@@ -33,10 +37,10 @@ struct CPUFeatures final {
 		movbe = !!(ecx & (1 << 22));
 		popcnt = !!(ecx & (1 << 23));
 		if ((ecx & (1 << 27)) && (ecx & (1 << 28))) {
-			eax = static_cast<decltype(eax)>(_xgetbv(0) & 0x00000000FFFFFFFFull);
+			eax = static_cast<decltype(eax + 0)>(_xgetbv(0) & 0x00000000FFFFFFFFull);
 			avx = ((eax & 0x6) == 0x6);
 			if (avx) {
-				__get_cpuid(7, &eax, &ebx, &ecx, &edx);
+				__cpuid(Registers, 7);
 				avx2 = !!(ebx & (1 << 5));
 			}
 		}
